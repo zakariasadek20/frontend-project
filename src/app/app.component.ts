1aac21dd7abc5892774cffffb95cd4bae42d2361
Zakaria Sadek <zakariasadek20@gmail.com>
2020-08-06 20:55:27 +0100

    fix error (import scripts in app.component.ts)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend-project';
+
+  //import scripts
+  loadScripts() {
+    const dynamicScripts = [
+      '../assets/js/script.js'
+    ];
+    for (let i = 0; i < dynamicScripts.length; i++) {
+      const node = document.createElement('script');
+      node.src = dynamicScripts[i];
+      node.type = 'text/javascript';
+      node.async = false;
+      node.charset = 'utf-8';
+      document.getElementsByTagName('head')[0].appendChild(node);
+    }
+  }
+  constructor() {
+    this.loadScripts();
+  }
 }
--- src/app/components/partials/home/home.component.ts
+++ src/app/components/partials/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as $ from 'jquery';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -10,6 +10,9 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    
   }
+
 
 }
